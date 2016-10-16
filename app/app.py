74b5840c332a092bd607bbdb8cf9b4556eec71ec
Jessica Lindee <jlindee@cs.utexas.edu>
2016-10-16 17:33:53 -0500

    add app folder

---
+++ app/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, url_for
+#from flask_script import Manager, Shell
+
+app = Flask(__name__)
+# manager = Manager(app)
+
+@app.route('/recipes')
+def recipes():
+	return render_template('recipes.html')
+
+@app.route('/ingredients')
+def ingredients():
+	return render_template('ingredients.html')
+
+@app.route('/cuisine')
+def cuisine():
+	return render_template('cuisine.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# def shell_context():
+# 	context = {
+# 		'app': app,
+# 		'db': db,
+# 		'Recipes': Recipes,
+# 		'Ingredients': Ingredients,
+# 		'Cuisines': Cuisine
+# 	}
+# 	return context
+
+
+# manager.add_command('shell', Shell(make_context=shell_context))
+
+if __name__ == "__main__":
+        app.run()
