e82f9acb59d9930426d87630642f553146f2ffa8
Anthony Gallop <agallop@cs.utexas.edu>
2016-10-20 01:53:31 -0500

    Getting rid of hard coding is hard

--- app/static/js/controller.js
+++ app/static/js/controller.js
@@ -12,4 +12,25 @@ mainApp.controller('CuisineDetailCtrl',
         $scope.cuisine = FetchFactory.fetchAt($routeParams['cuisineId']);
     }]);
 
+mainApp.controller('AboutCtrl',
+    [ '$scope', 'MemberFactory',
+    function($scope, MemberFactory) {
+        console.log("Member Factory fetch start");
+        members = MemberFactory.fetch();
+        $scope.seth = members[0];
+        $scope.alan = members[1];
+        $scope.anthony = members[2];
+        $scope.jessica = members[3];
+        $scope.zach = members[4];
+        console.log("Member Factory fetch end");
+    }]
+).directive('memberDetail', function() {
+  return {
+    scope: {
+      info: '=info'
+    },
+    templateUrl: '/static/partials/memberDetail.html'
+  }
+});;
+
 // Dependencies must be in the same order as the function paramters...
--- app/static/js/service.js
+++ app/static/js/service.js
@@ -6,7 +6,70 @@ mainApp.factory('FetchFactory', function() {
 		// { id: 3, title: 'Korean', numberOfRecipes: 23, averageNumberOfIngredientsPerRecipe: 6, continent:'Asia', averageCalories: 830, url: "https://media.timeout.com/images/100446787/image.jpg"},
 		// { id: 4, title: 'Vietnamese', numberOfRecipes: 62, averageNumberOfIngredientsPerRecipe: 9, continent:'Vietnam', averageCalories: 632, url: "https://www.cirquedusoleil.com/las-vegas/assets/img/extras/restaurants_noodles.jpg"},
 		];
-    // myFunction = function(){ console.log ('you called myFunction()')};  
+	// myFunction = function(){ console.log ('you called myFunction()')};  
+    return {
+      fetch: function () {
+      	return objectCache;
+      },
+      fetchAt: function (x) {
+      	return objectCache[x];
+      }
+  };
+});
+
+mainApp.factory('MemberFactory', function() {
+	var objectCache = [
+		{
+			id: 0, 
+			name:'Seth Milligan',
+			image: '/static/images/seth.jpeg', 
+			bio:'Hey everyone! My name is Seth and I’m a senior studying computer science at the University of Texas at Austin. Some of my hobbies include disc golf, playing board games, and listening to music.',
+			responsibilities:'Backend', 
+			commits: 12, 
+			issues:3, 
+			tests:2
+		},
+		{
+			id: 1, 
+			name:'Alan Ma', 
+			image: '/static/images/alan.jpg',
+			bio:'hi howdy hello i don\'t share feelings',
+			responsibilities:'Full Stack', 
+			commits: 12, 
+			issues:3, 
+			tests:2
+		},
+		{
+			id: 2, 
+			name:'Anthony Gallop', 
+			image: '/static/images/anthony.jpeg',
+			bio:'My name is Anthony Gallop (That\'s -op, not -up. I\'m suing the Gallup Poll for pain and suffering). My hobbies include playing video games and making bad chat bots. ',
+			responsibilities:'Frontend', 
+			commits: 12, 
+			issues:3, 
+			tests:2
+		},
+		{
+			id: 3, 
+			image: '/static/images/jessica.jpeg',
+			name:'Jessica Lindee', 
+			bio:'hi. 2spooky4me',
+			responsibilities:'wut', 
+			commits: 12, 
+			issues:3, 
+			tests:2
+		},
+		{
+			id: 4, 
+			image:'/static/images/zachary.jpg',
+			name:'Zach Gilkerson', 
+			bio:'yooo',
+			responsibilities:'1', 
+			commits: 12, 
+			issues:3, 
+			tests:2
+		}
+	];
     return {
       fetch: function () {
       	return objectCache;
